68d53ac6ff5bac50dc85986f9f811e498895784f
martynov <callmemars111@gmail.com>
2024-06-29 21:21:43 +0300

    resut methods v0.1.1

--- failure.go
+++ failure.go
@@ -7,6 +7,22 @@ type FailurePayload struct {
 	Message u.Optional[string]
 }
 
+type FailurePayloadMap map[string]string
+
+func (p FailurePayload) ToMap() FailurePayloadMap {
+	m := make(FailurePayloadMap)
+
+	if p.Code.HasValue() {
+		m["code"] = p.Code.Value()
+	}
+
+	if p.Message.HasValue() {
+		m["message"] = p.Message.Value()
+	}
+
+	return m
+}
+
 type Failure struct {
 	field   string
 	payload *FailurePayload
